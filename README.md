2f1d759c02dcd4cc65b0e0fcea88aa7786e8354a
Martin Podloucký <podlouckymartin@gmail.com>
2023-11-04 08:06:15 +0100

    Aktualizace struktury projektu

--- README.md
+++
@@ -1,3 +0,0 @@
-# Podklady pro cvičení „Hamburger menu“
-
-Postupujte podle [zadání](https://kodim.cz/kurzy/daweb/react/komunikace-dite-rodic/cv-komunikace#cvlekce%3Ehamburger-menu).
--- src/index.jsx
+++ src/index.jsx
@@ -1,35 +1,7 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './style.css';
+import { HomePage } from './pages/HomePage';
+import './global.css';
 
-const MenuItem = (props) => {
-  return (
-    <a href="#" className="menu-item">
-      {props.text}
-    </a>
-  );
-};
-
-const App = () => {
-  return (
-    <>
-      <header>
-        <div className="menu">
-          <button className="menu__btn"></button>
-          <div className="menu__items">
-            <MenuItem text="Domů" />
-            <MenuItem text="Naše nabídka" />
-            <MenuItem text="Náš tým" />
-            <MenuItem text="Blog" />
-            <MenuItem text="Kontakt" />
-          </div>
-        </div>
-      </header>
-      <main>
-        <h1>Domů</h1>
-      </main>
-    </>
-  );
-};
-
-createRoot(document.querySelector('#app')).render(<App />);
+createRoot(
+  document.querySelector('#app'),
+).render(<HomePage />);
