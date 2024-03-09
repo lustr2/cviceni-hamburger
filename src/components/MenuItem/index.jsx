94cb35ff851b2331aa3e2c8b7491dc3ea554af9a
Lucie Struplova <lstruplova@gmail.com>
2024-03-09 16:01:16 +0100

    Ukol HambgMenu z lekce7

--- src/components/MenuItem/index.jsx
+++ src/components/MenuItem/index.jsx
@@ -1,8 +1,14 @@
 import './style.css';
 
-export const MenuItem = ({ text }) => {
+export const MenuItem = ({ text, onSelect }) => {
+
+  const handleOnSelect = () => {
+    // misto onClick={() => onSelect()} pustime tuto fci
+    onSelect(text);
+  }
+
   return (
-    <a href="#" className="menu-item">
+    <a href="#" className="menu-item" onClick={handleOnSelect}>
       {text}
     </a>
   );
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,23 +1,36 @@
+import { useState } from 'react';
 import { MenuItem } from '../../components/MenuItem';
 import './style.css';
 
 export const HomePage = () => {
+  const [menuOpened, setMenuOpened] = useState(true);
+  const [pageTitle, setPageTitle] = useState('Domů');
+
+  const handleClickMenu = () =>{
+    setMenuOpened(!menuOpened);
+  }
+
+  const handleSelectItem = (page) => {
+    setMenuOpened(!menuOpened);
+    setPageTitle(page);
+  }
+
   return (
     <>
       <header>
-        <div className="menu">
-          <button className="menu__btn"></button>
+        <div className={menuOpened ? "menu" : "menu menu--closed"}>
+          <button className={menuOpened ? "menu__btn" : "menu__btn--closed"} onClick={handleClickMenu}></button>
           <div className="menu__items">
-            <MenuItem text="Domů" />
-            <MenuItem text="Naše nabídka" />
-            <MenuItem text="Náš tým" />
-            <MenuItem text="Blog" />
-            <MenuItem text="Kontakt" />
+            <MenuItem text="Domů" onSelect={handleSelectItem} />
+            <MenuItem text="Naše nabídka" onSelect={handleSelectItem} />
+            <MenuItem text="Náš tým" onSelect={handleSelectItem} />
+            <MenuItem text="Blog" onSelect={handleSelectItem} />
+            <MenuItem text="Kontakt" onSelect={handleSelectItem} />
           </div>
         </div>
       </header>
       <main>
-        <h1>Domů</h1>
+        <h1>{pageTitle}</h1>
       </main>
     </>
   );
